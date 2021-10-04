f008d5bb84d509a8e79763a061b51d0a81c2ddfc
zzzzz167 <2742400566@qq.com>
2021-10-04 12:03:26 +0800

    动效优化

--- blog/themes/sakura/source/js/main.js
+++ blog/themes/sakura/source/js/main.js
@@ -1,7 +1,14 @@
 function headertop_down() {
     var coverOffset = $('#content').offset().top;
     $('html,body').animate({
-        scrollTop: coverOffset
-    },
-    800);
+        scrollTop: coverOffset,
+        easing   : 'swing'
+    },800);
+}
+
+function getToTop(){
+    $('body,html').animate({
+        scrollTop: 0,
+        easing   : 'swing'
+      },800);
 }
