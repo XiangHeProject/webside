f249e3b41d5d04f910b2fe1f5f54e980cbdbdf80
zzzzz167 <2742400566@qq.com>
2021-12-19 12:15:08 +0800

    首页完工

--- blog/themes/sakura/source/js/boot.js
+++ blog/themes/sakura/source/js/boot.js
@@ -6,6 +6,9 @@ Sakura.boot.registerEvents = function(){
     Sakura.events.headertop_down();
     Sakura.events.get_to_top();
     Sakura.events.NH();
+    Sakura.events.MGT();
+    Sakura.events.MN();
+    Sakura.events.MNH();
 };
 
 document.addEventListener('DOMContentLoaded', function() {
--- blog/themes/sakura/source/js/events.js
+++ blog/themes/sakura/source/js/events.js
@@ -84,5 +84,62 @@ Sakura.events = {
             ss = s;
           }
         });
+      },
+      MGT: function () {
+        var offset = 20,
+          scroll_top_duration = 700,
+          $m_back_to_top = $(".m-cd-top");
+        $(window).scroll(function () {
+          if ($(this).scrollTop() > offset) {
+            $m_back_to_top.addClass("cd-is-visible");
+          } else {
+            $m_back_to_top.removeClass("cd-is-visible");
+          }
+        });
+        $m_back_to_top.on("click", function (event) {
+          event.preventDefault();
+          $("body,html").animate(
+            {
+              scrollTop: 0,
+            },
+            scroll_top_duration
+          );
+        });
+      },
+      MN: function () {
+        $(".iconflat").on("click", function () {
+          $("body").toggleClass("navOpen");
+          $("#main-container,#mo-nav,.openNav").toggleClass("open");
+        });
+      },
+  
+      // 移动端菜单自动隐藏
+      MNH: function () {
+        if ($("body").hasClass("navOpen")) {
+          $("body").toggleClass("navOpen");
+          $("#main-container,#mo-nav,.openNav").toggleClass("open");
+        }
+      },
+  
+      // 自适应窗口高度
+      AH: function () {
+        if (Poi.windowheight == "auto") {
+          if (window.outerWidth <= 860) {
+            $("#centerbg").css({ height: 300 });
+            $(".headertop").addClass("headertop-bar");
+            return;
+          }
+          $(".headertop").removeClass("headertop-bar");
+          if ($("h1.main-title").length > 0) {
+            var _height = $(window).height();
+            $("#centerbg").css({ height: _height });
+            $("#bgvideo").css({ "min-height": _height });
+            $(window).resize(function () {
+              Siren.AH();
+            });
+          }
+        } else {
+          $(".headertop").addClass("headertop-bar");
+        }
       }
 }
